849a5f9a687c2c9c386166a82f8f8c99f7fe8685
angelo-squadra <angelo.savioti@squadra.com.br>
2021-07-28 16:09:27 -0300

    created project

---
+++ android/app/src/main/kotlin/com/msavioti/estudo_social/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.msavioti.estudo_social
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:estudo_social/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
